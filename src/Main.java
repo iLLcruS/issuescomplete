b2ea923a69e3abd759d072388e10e4f1f5a320bd
vladmacbook <rostovskii.1030@gmail.com>
2023-03-09 20:16:32 +0200

    Added method MainMenu.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+    private String choose = "";
+    private static String password = "123";
+    private static String enterPassword = "";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello! Its a Issue Program\n "+
+                "Enter your password: ");
+        Scanner in = new Scanner(System.in);
+        if(enterPassword.contains(password)){
+        }
     }
 }
