e3798e1590a0e55d237deb9284931d9517dc018f
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 02:49:35 +0200

    Added VariableChooses.java class Added 3 object and 1 data type

---
+++ src/VariableChooses.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class VariableChooses {
+    Scanner scanner = new Scanner(System.in);
+    static MainMenu mm = new MainMenu();
+    protected static int enterChooseNumber;
+    protected static String chooseAction = "";
+}
