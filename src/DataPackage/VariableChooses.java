cdd1cc4f8075b51bb0ac48564c53e4c0c25b0e61
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 04:26:59 +0200

    Added getter and setter for enterChooseNumber; chooseAction

--- src/DataPackage/VariableChooses.java
+++ src/DataPackage/VariableChooses.java
@@ -6,6 +6,21 @@ import MainModules.MainMenu;
 public class VariableChooses {
     protected Scanner scanner = new Scanner(System.in);
     protected static MainMenu mm = new MainMenu();
+
     protected static int enterChooseNumber;
+    public static int getEnterChooseNumber() {
+        return enterChooseNumber;
+    }
+
+    public static void setEnterChooseNumber(int enterChooseNumber) {
+        VariableChooses.enterChooseNumber = enterChooseNumber;
+    }
+
     protected static String chooseAction = "";
+    public static String getChooseAction() {
+        return chooseAction;
+    }
+    public static void setChooseAction(String chooseAction) {
+        VariableChooses.chooseAction = chooseAction;
+    }
 }
