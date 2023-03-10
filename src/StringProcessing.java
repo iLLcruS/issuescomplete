121c47a47f4a6357f55cd18037f28d269deac1a5
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 02:47:23 +0200

    Refactoring code Add func back to MainMenu

--- src/StringProcessing.java
+++ src/StringProcessing.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 
-public class StringProcessing {
+public class StringProcessing extends VariableChooses {
+    private String userSentence;
+    private int numberOneForSubstring;
+    private int numberTwoForSubstring;
 
-    public void substringString(){
-        Scanner scanner = new Scanner(System.in);
+    public void substringString() {
         System.out.println("Input your sentence: ");
-        String userSentence = scanner.nextLine();
+        userSentence = scanner.nextLine();
+
         System.out.println("Input the first number: ");
-        int numberOneForSubstring = scanner.nextInt();
+        numberOneForSubstring = scanner.nextInt();
+
         System.out.println("Input the second number: ");
-        int numberTwoForSubstring = scanner.nextInt();
+        numberTwoForSubstring = scanner.nextInt();
+
         System.out.println("Your final sentence is: ");
-        System.out.println(userSentence.substring(numberOneForSubstring,numberTwoForSubstring));
+        System.out.println(userSentence.substring(numberOneForSubstring, numberTwoForSubstring));
+
+        System.out.println("Do you want to back Main Menu?(1 - yes; 2 - no)");
+        enterChooseNumber = scanner.nextInt();
+
+        if (enterChooseNumber == 1) {
+            mm.Menu();
+        } else if (enterChooseNumber == 2) {
+            substringString();
+        }
     }
 }
