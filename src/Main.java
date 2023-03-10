5553b2d2a9bea4d206be8c56e6a1c6ea89699557
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 02:21:49 +0200

    Validation password(else-if)

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,18 @@ public class Main {
     private String choose = "";
     private static String password = "123";
     private static String enterPassword = "";
-   static MainMenu mm = new MainMenu();
+    static MainMenu mm = new MainMenu();
 
     public static void main(String[] args) {
-        System.out.println("Hello! Its a Issue Program\n "+
+        System.out.println("Hello! Its a Issue Program\n " +
                 "Enter your password: ");
         Scanner in = new Scanner(System.in);
         enterPassword = in.nextLine();
-        if(enterPassword.contains(password)){
+        if (enterPassword.contains(password)) {
             mm.Menu();
+
+        } else if (!enterPassword.contains(password)) {
+            System.out.println("Wrong password, try again!");
         }
     }
 }
