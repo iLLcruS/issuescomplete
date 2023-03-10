1bbfa320d695f4ce0b222e6f003d1f0d3bc3f94c
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 05:24:04 +0200

    Changed to setter sentences and numbers, extends StringProcessingAbs.java

--- src/MainModules/StringProcessing.java
+++ src/MainModules/StringProcessing.java
@@ -1,34 +1,30 @@
 package MainModules;
 
+import AbstractClasses.StringProcessingAbs;
 import DataPackage.VariableChooses;
 
 import java.util.Scanner;
-
-public class StringProcessing extends VariableChooses {
-    private String userSentence;
-    private int numberOneForSubstring;
-    private int numberTwoForSubstring;
-
-    public void substringString() {
+public class StringProcessing extends StringProcessingAbs {
+    public void substringString(){
         System.out.println("Input your sentence: ");
-        userSentence = scanner.nextLine();
+        setUserSentence(scanner.next());
 
         System.out.println("Input the first number: ");
-        numberOneForSubstring = scanner.nextInt();
+        setNumberOneForSubstring(scanner.nextInt());
 
         System.out.println("Input the second number: ");
-        numberTwoForSubstring = scanner.nextInt();
+        setNumberTwoForSubstring(scanner.nextInt());
 
         System.out.println("Your final sentence is: ");
         System.out.println(userSentence.substring(numberOneForSubstring, numberTwoForSubstring));
 
         System.out.println("Do you want to back Main Menu?(1 - yes; 2 - no)");
-        enterChooseNumber = scanner.nextInt();
-
+        setEnterChooseNumber(scanner.nextInt());
         if (enterChooseNumber == 1) {
             mm.Menu();
         } else if (VariableChooses.enterChooseNumber == 2) {
             substringString();
         }
     }
+
 }
