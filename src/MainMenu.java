561b603de96d3c7b43b0cb11c496e1ee6293d478
vladmacbook <rostovskii.1030@gmail.com>
2023-03-10 02:20:14 +0200

    Added func "calc" to main functions(work)

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,22 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainMenu {
     private String chooseAction = "";
     StringProcessing sp = new StringProcessing();
+    Calculator calc = new Calculator();
     Scanner input = new Scanner(System.in);
 
-    public void Menu(){
-        System.out.println("Welcome to the program!\n "+
+    static MainMenu mm = new MainMenu();
+
+    public void Menu() {
+        System.out.println("Welcome to the program!\n " +
                 "Choose your command: ");
         chooseAction = input.nextLine();
-        if(chooseAction.contains("string")){
-            System.out.println("Available methods for string:\n "+
+        if (chooseAction.contains("string")) {
+            System.out.println("Available methods for string:\n " +
                     "Substring");
             chooseAction = input.next();
-            if(chooseAction.contains("Substring")){
+            if (chooseAction.contains("Substring")) {
                 sp.substringString();
                 Menu();
             }
+        }
+        if (chooseAction.contains("calc")) {
+            calc.calculator();
+            Menu();
         }
     }
 }
