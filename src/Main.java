36776b81f9555487b96a1efb9bffe49f0ed218c6
KamikottoGG <123205307+KamikottoGG@users.noreply.github.com>
2023-06-05 16:29:10 +0300

    Moved method new Logger().logWriter(); to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import Modules.CheckFilesAndDirectory;
 import Modules.Checks;
+import Modules.Logger;
 import Modules.Visual;
 
 public class Main{
     public static void main(String[] args) {
+        new Logger().logWriter();
+        new CheckFilesAndDirectory().allCheck();
         new Visual().printMsgWithProgressBar("Loading", 25, 60);
         System.out.println("\nHello! Its a Issue Program\n ");
-        new CheckFilesAndDirectory().allCheck();
+
         new Checks().checkPassword();
+
     }
 }
--- src/Modules/Checks.java
+++ src/Modules/Checks.java
@@ -12,11 +12,11 @@ public class Checks {
 
     public void checkPassword(){
 
-        System.out.println("Enter your password: ");
+        System.out.print("Enter your password: ");
         Scanner in = new Scanner(System.in);
         enterPassword = in.nextLine();
         if (enterPassword.contains(password)) {
-            new Logger().logWriter();
+
             new MainMenu().Menu();
         } else if (!enterPassword.contains(password)) {
             System.out.println("Wrong password, try again!");
