210093fae6748970c5022cd20d50745690cc6630
vladmacbook <rostovskii.1030@gmail.com>
2023-06-05 14:22:43 +0300

    Added nameProperty to class Variable.java

--- src/DataPackage/Variable.java
+++ src/DataPackage/Variable.java
@@ -2,31 +2,23 @@ package DataPackage;
 
 public class Variable extends VariableChooses{
     protected double firstNumber;
-
     public double getFirstNumber() {
         return firstNumber;
     }
-
     public void setFirstNumber(double firstNumber) {
         this.firstNumber = firstNumber;
     }
-
     protected double secondNumber;
-
     public double getSecondNumber() {
         return secondNumber;
     }
-
     public void setSecondNumber(double secondNumber) {
         this.secondNumber = secondNumber;
     }
-
     protected double result;
-
     public double getResult() {
         return result;
     }
-
     public void setResult(double result) {
         this.result = result;
     }
@@ -34,11 +26,9 @@ public class Variable extends VariableChooses{
     public String getUserSentenceAppend() {
         return userSentenceAppend;
     }
-
     public void setUserSentenceAppend(String userSentenceAppend) {
         this.userSentenceAppend = userSentenceAppend;
     }
-
     protected String userSentence;
     public String getUserSentence() {
         return userSentence;
@@ -46,18 +36,13 @@ public class Variable extends VariableChooses{
     public void setUserSentence(String userSentence) {
         this.userSentence = userSentence;
     }
-
     protected int numberOneForSubstring;
-
     public void setNumberOneForSubstring(int numberOneForSubstring) {
         this.numberOneForSubstring = numberOneForSubstring;
     }
     protected int numberTwoForSubstring;
-
     public void setNumberTwoForSubstring(int numberTwoForSubstring) {
         this.numberTwoForSubstring = numberTwoForSubstring;
     }
-
-
-
+    public String nameProperty = System.getProperty("user.name");
 }
