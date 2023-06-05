7be93c239b809682d550f5dda317edbc48798b3e
vladmacbook <rostovskii.1030@gmail.com>
2023-06-05 05:15:55 +0300

    Add Checks.java for all check in program

---
+++ src/Modules/Checks.java
@@ -0,0 +1,25 @@
+package Modules;
+
+import DataPackage.VariableChooses;
+import MainModules.MainMenu;
+
+import java.util.Scanner;
+
+public class Checks {
+    private String choose = "";
+    private static String password = "123";
+    private static String enterPassword = "";
+
+    public void checkPassword(){
+
+        System.out.println("Enter your password: ");
+        Scanner in = new Scanner(System.in);
+        enterPassword = in.nextLine();
+        if (enterPassword.contains(password)) {
+            new MainMenu().Menu();
+        } else if (!enterPassword.contains(password)) {
+            System.out.println("Wrong password, try again!");
+            checkPassword();
+        }
+    }
+}
