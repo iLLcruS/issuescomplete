f5c183a17f0b2accd6431ec57e712c7fd2015fd6
vladmacbook <rostovskii.1030@gmail.com>
2023-06-08 02:02:50 +0300

    Added discriminant function

--- src/MainModules/AbstractClasses/CalculateAbs.java
+++ src/MainModules/AbstractClasses/CalculateAbs.java
@@ -6,7 +6,9 @@ public abstract class CalculateAbs extends Variable {
    public abstract double sum(double x, double y);
 
    public abstract void sumComplete();
+
    public abstract double minus(double x, double y);
+
    public abstract void minusComplete();
 
    public abstract double multiplication(double x, double y);
@@ -22,20 +24,32 @@ public abstract class CalculateAbs extends Variable {
    public abstract void interestComplete();
 
    public abstract double sqrt(double x);
+
    public abstract void sqrtComplete();
+
    public abstract double pow(double x, double y);
+
    public abstract void powComplete();
 
    public abstract double sin(double x);
+
    public abstract void sinComplete();
 
    public abstract double cos(double x);
+
    public abstract void cosComplete();
 
 
+   public abstract double x2(double dis, double a, double b);
+
+   public abstract double x(double b, double a);
+
+   public abstract void disc();
+
+   public abstract double discriminant(double b, double a, double c);
+
+   public abstract double x1(double dis, double a, double b);
 }
-
-
 
 
 
--- src/MainModules/Calculator.java
+++ src/MainModules/Calculator.java
@@ -140,6 +140,7 @@ public class Calculator extends CalculateAbs {
     public double cos(double x) {
         return Math.cos(x);
     }
+
     public void cosComplete() {
         Scanner scanner = new Scanner(System.in);
 
@@ -148,6 +149,59 @@ public class Calculator extends CalculateAbs {
 
         out.println("res: " + cos(getFirstNumber()));
         new MainMenu().Menu();
+    }
+
+    @Override
+    public double discriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
+    }
+
+    @Override
+    public double x1(double dis, double a, double b) {
+        return (-b - Math.sqrt(dis)) / (2 * a);
+    }
+
+    @Override
+    public double x2(double dis, double a, double b) {
+        return (-b + Math.sqrt(dis)) / (2 * a);
+    }
+
+    @Override
+    public double x(double b, double a) {
+        return -b / (2 * a);
+    }
+
+    @Override
+    public void disc() {
+
+        out.println("Enter a value: ");
+        double a = scanner.nextDouble();
+
+        out.println("Enter b value: ");
+        double b = scanner.nextDouble();
+
+        out.println("Enter c value: ");
+        double c = scanner.nextDouble();
+
+        double disc = discriminant(a, b, c);
+        if (disc > 0) {
+            double x1 = x1(disc, a, b);
+            double x2 = x2(disc, a, b);
+            out.println("The equation has 2 roots");
+            out.println("D = " + disc);
+            out.println("x1= " + x1);
+            out.println("x2= " + x2);
+            new MainMenu().Menu();
+        } else if (disc == 0) {
+            double x = x(b, a);
+            out.println("The equation has 1 roots");
+            out.println("D = " + disc);
+            out.println("x= " + x);
+            new MainMenu().Menu();
+        } else if (disc < 0) {
+            out.println("The equation has 0 roots ");
+            new MainMenu().Menu();
+        }
     }
 }
 
