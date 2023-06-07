f647f2eb9bcb51161310082b7dc847d830c7f2ff
vladmacbook <rostovskii.1030@gmail.com>
2023-06-08 02:19:25 +0300

    Refactoring

--- src/DataPackage/Variable.java
+++ src/DataPackage/Variable.java
@@ -1,48 +1,67 @@
 package DataPackage;
 
 public class Variable extends VariableChooses{
+
     protected double firstNumber;
+
     public double getFirstNumber() {
         return firstNumber;
     }
+
     public void setFirstNumber(double firstNumber) {
         this.firstNumber = firstNumber;
     }
+
     protected double secondNumber;
+
     protected char charOne, charTwo;
 
     public double getSecondNumber() {
         return secondNumber;
     }
+
     public void setSecondNumber(double secondNumber) {
         this.secondNumber = secondNumber;
     }
+
     protected double result;
+
     public double getResult() {
         return result;
     }
+
     public void setResult(double result) {
         this.result = result;
     }
+
     protected String userSentenceAppend;
+
     public String getUserSentenceAppend() {
         return userSentenceAppend;
     }
+
     public void setUserSentenceAppend(String userSentenceAppend) {
         this.userSentenceAppend = userSentenceAppend;
     }
+
     protected String userSentence;
+
     public String getUserSentence() {
         return userSentence;
     }
+
     public void setUserSentence(String userSentence) {
         this.userSentence = userSentence;
     }
+
     protected int numberOneForSubstring;
+
     public void setNumberOneForSubstring(int numberOneForSubstring) {
         this.numberOneForSubstring = numberOneForSubstring;
     }
+
     protected int numberTwoForSubstring;
+
     public void setNumberTwoForSubstring(int numberTwoForSubstring) {
         this.numberTwoForSubstring = numberTwoForSubstring;
     }
@@ -51,17 +70,21 @@ public class Variable extends VariableChooses{
     {
         this.charOne = charOne;
     }
+
     public char getCharOne()
     {
         return charOne;
     }
+
     public void setCharTwo(char charTwo)
     {
         this.charTwo = charTwo;
     }
+
     public char getCharTwo()
     {
         return charTwo;
     }
+
     public String nameProperty = System.getProperty("user.name");
 }
