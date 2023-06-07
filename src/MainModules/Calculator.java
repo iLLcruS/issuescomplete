7936f91bb764d223f6d4a9d3accc43d166b7d810
vladmacbook <rostovskii.1030@gmail.com>
2023-06-08 00:06:53 +0300

    Realize function Calculator.sum()

--- src/MainModules/Calculator.java
+++ src/MainModules/Calculator.java
@@ -3,10 +3,26 @@ package MainModules;
 
 import MainModules.AbstractClasses.CalculateAbs;
 
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
 public  class Calculator extends CalculateAbs {
 
-    public int sum(int x, int y){
-        return x+y;
+    public double sum(double x, double y){
+        return x + y;
+    }
+    public void sumComplete(){
+        Scanner scanner = new Scanner(System.in);
+
+        out.println("Enter your first number: ");
+        setFirstNumber(scanner.nextDouble());
+
+        out.println("Enter your second number: ");
+        setSecondNumber(scanner.nextDouble());
+
+        out.println("Sum: " + sum(getFirstNumber(), getSecondNumber()));
+        new MainMenu().Menu();
     }
 }
 
