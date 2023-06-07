160ef1d60b0340b013de9b940268c956a83f1db6
0iqgenius <raildoter@gmail.com>
2023-06-08 00:26:39 +0300

    changed place methods

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import static java.lang.System.out;
 
 public class Main{
     public static void main(String[] args) {
-        new Logger().logWriter();
         new CheckFilesAndDirectory().allCheck();
+        new Logger().logWriter();
         new Visual().printMsgWithProgressBar("Loading", 25, 60);
         out.println("\nHello! Its a Issue Program\n ");
         new Checks().checkPassword();
