5ed57ab25db9a560256111770b5a8270fc054072
vladmacbook <rostovskii.1030@gmail.com>
2023-06-07 16:33:49 +0300

    Added log-session-reader and log-command-reader

---
+++ src/MainModules/LogReader.java
@@ -0,0 +1,24 @@
+package MainModules;
+
+import Modules.CheckFilesAndDirectory;
+
+import java.io.*;
+
+public class LogReader {
+    private void logSessionReader() throws IOException {
+        File f = new File(new CheckFilesAndDirectory().getPathLogFile());
+        BufferedReader fin = new BufferedReader(new FileReader(f));
+        String line;
+        while ((line = fin.readLine()) != null) System.out.println(line);
+    }
+    private void logCommandReader() throws IOException{
+        File file = new File(new CheckFilesAndDirectory().getPathLogFile());
+        BufferedReader fin = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = fin.readLine()) != null) System.out.println(line);
+    }
+    public void allLogReader() throws IOException{
+        logCommandReader();
+        logSessionReader();
+    }
+}
