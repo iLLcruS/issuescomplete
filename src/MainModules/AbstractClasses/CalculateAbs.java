628300fd9b428d4e0f3b99f2c7264f4b6c638439
0iqgenius <raildoter@gmail.com>
2023-06-07 23:46:28 +0300

    moved to another package

---
+++ src/MainModules/AbstractClasses/CalculateAbs.java
@@ -0,0 +1,18 @@
+package MainModules.AbstractClasses;
+
+import DataPackage.Variable;
+
+public abstract class CalculateAbs extends Variable {
+   public abstract int sum(int x, int y);
+
+
+}
+
+
+
+
+
+
+
+
+
--- src/MainModules/Calculator.java
+++ src/MainModules/Calculator.java
@@ -1,66 +1,16 @@
 package MainModules;
 
-import AbstractClasses.CalculateAbs;
-import java.util.Scanner;
 
-public class Calculator extends CalculateAbs {
+import MainModules.AbstractClasses.CalculateAbs;
 
-    public void calculator() {
+public  class Calculator extends CalculateAbs {
 
-        System.out.println("How many mathematical operations do you need to perform?");
-        int countRepeat = scanner.nextInt();
-        if (countRepeat > 20) {
-            System.out.println("Sorry, many operations.");
-            calculator();
-        } else {
-            for (int i = 0; i < countRepeat; i++) {
-
-                System.out.print("Enter first num: ");
-                firstNumber = scanner.nextDouble();
-
-                System.out.print("Enter second numb: ");
-                secondNumber = scanner.nextDouble();
-
-
-                System.out.print("Enter an available operator (+, -, *, /) : ");
-
-                char operator = scanner.next().charAt(0);
-
-
-                switch (operator) {
-                    case '+':
-                        result = firstNumber + secondNumber;
-                        setResult(result);
-                        break;
-                    case '-':
-                        result = firstNumber - secondNumber;
-                        setResult(result);
-                        break;
-                    case '*':
-                        result = firstNumber * secondNumber;
-                        setResult(result);
-                        break;
-                    case '/':
-                        result = firstNumber / secondNumber;
-                        setResult(result);
-                        break;
-                    default:
-                        System.out.println("Invalid operator.");
-                        mm.Menu();
-                        return;
-                }
-                System.out.println("Result: " + result);
-            }
-        }
-        System.out.println("Do you want to back Main Menu?(1 - yes; 2 - no)");
-        setEnterChooseNumber(scanner.nextInt());
-        if (enterChooseNumber == 1) {
-            mm.Menu();
-        } else if (enterChooseNumber == 2) {
-            calculator();
-        }
+    public int sum(int x, int y){
+        return x+y;
     }
 }
+
+
 
 
 
