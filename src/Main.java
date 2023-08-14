2ff06dc48196f4119d98d174a49307c3158ba443
Anna Mamysheva <82759145+amamysheva@users.noreply.github.com>
2023-08-15 02:29:56 +0300

    Added class for change console color

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import Modules.AllChecks.CheckFilesAndDirectory;
 import Modules.AllChecks.CheckSession;
-import Modules.AllChecks.Checks;
-import Modules.JsonSession;
 import Modules.Logger;
-import Modules.Visual;
+import Modules.TestLesson;
+import Modules.VisualForConsole.ChangeConsoleColor;
+import Modules.VisualForConsole.Visual;
 
 import static java.lang.System.out;
 
@@ -11,7 +11,10 @@ public class Main{
     public static void main(String[] args){
         new CheckFilesAndDirectory().allCheck();
         new Logger().logWriter();
+        new TestLesson().testing();
+        new ChangeConsoleColor().setRedColor();
         new Visual().printMsgWithProgressBar("Loading", 25, 60);
+        new ChangeConsoleColor().setGreenColor();
         out.println("\nHello! Its a Issue Program\n ");
         new CheckSession().checkSession();
     }
